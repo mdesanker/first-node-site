e1f039cc1a67ea2309e4902c196dd4c7c67e1685
Michael Desanker <mdesanker@gmail.com>
2021-12-23 10:47:46 -0500

    Use links to navigate pages

--- index.js
+++ index.js
@@ -5,11 +5,31 @@ const port = process.env.PORT || 8080;
 
 const server = http
   .createServer((req, res) => {
-    fs.readFile("./index.html", (err, data) => {
-      res.writeHead(200, { "Content-Type": "text/html" });
-      res.write(data);
-      res.end();
-    });
+    if (req.url === "/") {
+      fs.readFile(`./index.html`, (err, data) => {
+        res.writeHead(200, { "Content-Type": "text/html" });
+        res.write(data);
+        res.end();
+      });
+    } else if (req.url === "/about") {
+      fs.readFile(`./about.html`, (err, data) => {
+        res.writeHead(200, { "Content-Type": "text/html" });
+        res.write(data);
+        res.end();
+      });
+    } else if (req.url === "/contact") {
+      fs.readFile(`./contact.html`, (err, data) => {
+        res.writeHead(200, { "Content-Type": "text/html" });
+        res.write(data);
+        res.end();
+      });
+    } else {
+      fs.readFile(`./404.html`, (err, data) => {
+        res.writeHead(200, { "Content-Type": "text/html" });
+        res.write(data);
+        res.end();
+      });
+    }
   })
   .listen(port, () => {
     console.log(`Server running at port ${port}`);
