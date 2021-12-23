6386c195181f6baeddb035ae0eff77e3b42b1517
Michael Desanker <mdesanker@gmail.com>
2021-12-23 12:49:58 -0500

    Add error catching

--- index.js
+++ index.js
@@ -7,24 +7,28 @@ const server = http
   .createServer((req, res) => {
     if (req.url === "/") {
       fs.readFile(`./index.html`, (err, data) => {
+        if (err) throw err;
         res.writeHead(200, { "Content-Type": "text/html" });
         res.write(data);
         res.end();
       });
     } else if (req.url === "/about") {
       fs.readFile(`./about.html`, (err, data) => {
+        if (err) throw err;
         res.writeHead(200, { "Content-Type": "text/html" });
         res.write(data);
         res.end();
       });
     } else if (req.url === "/contact") {
       fs.readFile(`./contact.html`, (err, data) => {
+        if (err) throw err;
         res.writeHead(200, { "Content-Type": "text/html" });
         res.write(data);
         res.end();
       });
     } else {
       fs.readFile(`./404.html`, (err, data) => {
+        if (err) throw err;
         res.writeHead(200, { "Content-Type": "text/html" });
         res.write(data);
         res.end();
